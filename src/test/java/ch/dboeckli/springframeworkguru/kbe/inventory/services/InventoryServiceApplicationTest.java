019c69ca63089f718b467cd990f01c533480ca8f
dboeckli <dboeckli@gmail.com>
2025-02-09 08:44:02 +0100

    refactor project

--- src/test/java/ch/dboeckli/springframeworkguru/kbe/inventory/services/InventoryServiceApplicationTest.java
+++ src/test/java/ch/dboeckli/springframeworkguru/kbe/inventory/services/InventoryServiceApplicationTest.java
@@ -3,8 +3,10 @@ package ch.dboeckli.springframeworkguru.kbe.inventory.services;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
+@ActiveProfiles("it-test")
 @Slf4j
 class InventoryServiceApplicationTest {
 

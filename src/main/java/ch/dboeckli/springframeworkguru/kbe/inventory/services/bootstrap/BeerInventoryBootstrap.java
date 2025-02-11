b15b75a3b67b223818be7bcdcaab211f1edbb872
adm-boeckli <dboeckli@gmail.com>
2025-02-11 12:52:04 +0100

    use hikari config and do not set uuid. they are generated

--- src/main/java/ch/dboeckli/springframeworkguru/kbe/inventory/services/bootstrap/BeerInventoryBootstrap.java
+++ src/main/java/ch/dboeckli/springframeworkguru/kbe/inventory/services/bootstrap/BeerInventoryBootstrap.java
@@ -7,11 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.UUID;
-
-/**
- * Created by jt on 2019-06-07.
- */
 @Slf4j
 @RequiredArgsConstructor
 @Component
@@ -19,9 +14,6 @@ public class BeerInventoryBootstrap implements CommandLineRunner {
     private static final String BEER_1_UPC = "0631234200036";
     private static final String BEER_2_UPC = "0631234300019";
     private static final String BEER_3_UPC = "0083783375213";
-    private static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
-    private static final UUID BEER_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
-    private static final UUID BEER_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
 
     private final BeerInventoryRepository beerInventoryRepository;
 
@@ -36,21 +28,18 @@ public class BeerInventoryBootstrap implements CommandLineRunner {
         log.info("Loading initial beer inventory...");
         beerInventoryRepository.save(BeerInventory
                 .builder()
-                .beerId(BEER_1_UUID)
                 .upc(BEER_1_UPC)
                 .quantityOnHand(10)
                 .build());
 
         beerInventoryRepository.save(BeerInventory
                 .builder()
-                .beerId(BEER_2_UUID)
                 .upc(BEER_2_UPC)
                 .quantityOnHand(10)
                 .build());
 
         beerInventoryRepository.save(BeerInventory
                 .builder()
-                .beerId(BEER_3_UUID)
                 .upc(BEER_3_UPC)
                 .quantityOnHand(50)
                 .build());
